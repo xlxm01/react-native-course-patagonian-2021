5427f3f4a1d658d16f60e109aaff7ee1af87cc34
Laura <51368962+xlxm01@users.noreply.github.com>
2021-07-24 21:39:09 -0300

    Componente View
    Primera clase parte 1

--- App.tsx
+++ App.tsx
@@ -1,57 +1,14 @@
 import React from 'react';
-import { Alert, Image, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { DefaultButton } from './src/components';
-
-import { colors } from './src/utils/theme';
-import { DEVICE_WIDTH } from './src/utils/dimensions';
-
-const showAlert = () => {
-  Alert.alert('Hola !!!');
-};
+const hola = { background: 'blue', heigh: 100, width: 100 }
 
 const App = () => {
-  //console.log('Device height -> ', DEVICE_HEIGHT);
-  //console.log('Device width -> ', DEVICE_WIDTH);
-  return (
-    <View style={styles.mainContainer}>
-      <Text style={styles.title}>Hola Mundo!!</Text>
-      <Text style={styles.subtitle}>Laura Murillo</Text>
-      <Image
-        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <DefaultButton onPress={showAlert} msg={'Hola'} />
-    </View>
-  );
+  return <View style={hola} />;
 };
 
-const styles = StyleSheet.create({
-  mainContainer: {
-    alignItems: 'center',
-    backgroundColor: colors.verylightBlue,
-    flex: 1,
-    justifyContent: 'center',
-    width: '100%',
-  },
-  title: {
-    color: colors.mainOrange,
-    fontSize: 50,
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-    marginTop: 100,
-    marginBottom: 10,
-  },
-  image: {
-    minHeight: 100,
-    width: DEVICE_WIDTH * 0.5,
-    marginTop: 100,
-  },
-  subtitle: {
-    color: 'white',
-    fontSize: 25,
-  }
+const style = StyleSheet.create({
+
 });
 
 export default App;
