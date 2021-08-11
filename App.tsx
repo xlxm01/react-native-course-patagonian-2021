449e84ffc1b895c4860e283b1676374e25daec65
Laura <51368962+xlxm01@users.noreply.github.com>
2021-08-11 16:05:03 -0300

    Detalles del default button

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Image, Modal, StyleSheet, Text, View } from 'react-native';
 
 import { DefaultBotton } from './src/components';
-import Typography from './src/components/Typography';
+//import Typography from './src/components/Typography';
 
 import { colors } from './src/utils/theme';
 import { DEVICE_WIDTH } from './src/utils/dimensions';
@@ -52,15 +52,21 @@ const App = () => {
       </Modal>
       <Text style={styles.title}>LAURA MURILLO</Text>
       <Image
-        style={styles.logo}
         resizeMode="contain"
+        style={styles.logo}
         source={{
           uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTmNUH9WwaxR9MplaF9ko2Z3rWkTiGYwDGR0kMKeZ7zr7adktK0aiQgJmMb_lVBk0m84&usqp=CAU',
         }}
       />
       <Text style={styles.subtitle}>Team Leader</Text>
-      <Typography> Team Leader </Typography>
-      <DefaultBotton text="Mas Info" onPress={showModal} textSize={14} />
+      {/* <Typography> Team Leader </Typography> */}
+      <DefaultBotton
+        color={colors.mainBoton} // este color va a sobreescribir cualquier color que uno le mande, incluso en el aditional styles
+        //additionalStyle={styles.button}
+        text="Mas Info"
+        onPress={showModal}
+        textSize={22}
+      />
     </View>
   );
 };
@@ -90,6 +96,10 @@ const styles = StyleSheet.create({
     minHeight: 170,
     marginTop: 15,
     width: DEVICE_WIDTH * 0.5,
+  },
+  button: {
+    backgroundColor: 'green',
+    width: 50,
   },
 });
 
--- src/components/DefaultBotton/DefaultBotton.tsx
+++ src/components/DefaultBotton/DefaultBotton.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, ViewStyle } from 'react-native';
+import { colors } from '../../utils/theme';
 
 import styles from './styles';
 
 interface Props {
-  additionalStyle?: object;
+  additionalStyle?: ViewStyle;
+  color?: string;
   onPress: () => void;
   text: string;
   textSize?: number;
   variant?: 'primary' | 'secondary';
 }
 
-const DefaultBotton = ({ additionalStyle, onPress, text, textSize }: Props) => {
+const DefaultBotton = ({ additionalStyle, color, onPress, text, textSize }: Props) => {
   return (
-    <TouchableOpacity style={[styles.mainContainer, additionalStyle]} onPress={onPress}>
+    <TouchableOpacity
+      style={[styles.mainContainer, additionalStyle, { backgroundColor: color }]}
+      onPress={onPress}
+    >
+      {/* Cada vez que le llegue algo en textSize va a tener que recalcular
+      no importa que sea lo mismo, si fuera algo fijo como fontSize: 10 estaría mal */}
       <Text style={{ fontSize: textSize }}> {text} </Text>
     </TouchableOpacity>
   );
@@ -21,7 +28,8 @@ const DefaultBotton = ({ additionalStyle, onPress, text, textSize }: Props) => {
 // Define propiedades por defecto
 DefaultBotton.defaultProps = {
   additionalStyle: {},
-  textSize: 12,
+  color: colors.mainBoton,
+  textSize: 14,
 };
 
 export default DefaultBotton;
